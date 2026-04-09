1ad4775d9fe8878f908ce3ae30dd51fd7de89fca
Garrison Taylor <garrison.m.taylor@gmail.com>
2026-04-08 20:40:23 -0500

    Add some simple exception handling and test ecall/mret

--- src/main.c
+++ src/main.c
@@ -1,11 +1,22 @@
 extern int num;
 int i;
 
+void halt();
+extern void ecall(unsigned int);
+
 void init()
 {
         i = num;
+        ecall(i);
+        halt();
 }
 
 void handle_interrupt()
-{}
+{
 
+}
+
+void halt()
+{
+        while (1);
+}
