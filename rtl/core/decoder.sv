2494537a6ab0c2f67bc07cd4977d5aa1bc586b4b
Garrison Taylor <garrison.m.taylor@gmail.com>
2026-05-09 15:24:06 -0500

    Move old core code and write a decoder in SV
    All old Verilog code is being archived for now, and each stage is being
    written and tested one by one in SystemVerilog, starting with the
    decoder. This commit also introduces rv32.sv, a package containing
    typedefs for managing control words.

---
+++ rtl/core/decoder.sv
@@ -0,0 +1,189 @@
+module decoder
+import rv32::*;
+#(
+)(
+        input clk,
+        input rst,
+        input flush,
+        input stall,
+
+        input logic [31:0] instr_i,
+
+        output logic [4:0] rs1_o,
+        output logic [4:0] rs2_o,
+        output logic [31:0] imm_o,
+        output logic [4:0] rd_o,
+
+        output ctrl_t ctrl_o
+);
+
+
+instr_t instr;
+logic [4:0] rs1_next;
+logic [4:0] rs2_next;
+logic [31:0] imm_next;
+logic [4:0] rd_next;
+ctrl_t ctrl_next;
+
+// Pack raw instruction into union struct
+assign instr = instr_i;
+
+// Extract register identifiers (always same location)
+assign rs1_next = instr.r.rs1;
+assign rs2_next = instr.r.rs2;
+assign rd_next = instr.r.rd;
+
+// Extract opcode
+assign ctrl_next.opcode = instr.r.opcode;
+
+// Combinational decode step
+always_comb
+begin
+        // Default to NOP
+        imm_next = 0;
+        ctrl_next.alu_op = ADDSUB;
+        ctrl_next.alu_src1 = ZERO;
+        ctrl_next.alu_src2 = IMM;
+        ctrl_next.branch = 0;
+        ctrl_next.jump = 0;
+        ctrl_next.load = 0;
+        ctrl_next.store = 0;
+        ctrl_next.wb = 0;
+
+        unique case (instr.r.opcode)
+                LUI: begin
+                        imm_next[31:12] = instr.u.imm31_12;
+                        
+                        ctrl_next.alu_op = ADDSUB;
+                        ctrl_next.alu_src1 = ZERO;
+                        ctrl_next.alu_src2 = IMM;
+
+                        ctrl_next.wb = 1;
+                        ctrl_next.wb_src = ALU;
+                end
+
+                AUIPC: begin
+                        imm_next[31:12] = instr.u.imm31_12;
+                        
+                        ctrl_next.alu_op = ADDSUB;
+                        ctrl_next.alu_src1 = PC;
+                        ctrl_next.alu_src2 = IMM;
+
+                        ctrl_next.wb = 1;
+                        ctrl_next.wb_src = ALU;
+                end
+
+                JAL: begin
+                        imm_next[20] = instr.j.imm20;
+                        imm_next[19:12] = instr.j.imm19_12;
+                        imm_next[11] = instr.j.imm11;
+                        imm_next[10:1] = instr.j.imm10_1;
+
+                        ctrl_next.alu_op = ADDSUB;
+                        ctrl_next.alu_src1 = PC;
+                        ctrl_next.alu_src2 = IMM;
+
+                        ctrl_next.jump = 1;
+
+                        ctrl_next.wb = 1;
+                        ctrl_next.wb_src = PC4;
+                end
+
+                JALR: begin
+                        imm_next[11:0] = instr.i.imm11_0;
+
+                        ctrl_next.alu_op = ADDSUB;
+                        ctrl_next.alu_src1 = RS1;
+                        ctrl_next.alu_src2 = IMM;
+
+                        ctrl_next.jump = 1;
+
+                        ctrl_next.wb = 1;
+                        ctrl_next.wb_src = PC4;
+                end
+
+                BRANCH: begin
+                        imm_next[12] = instr.b.imm12;
+                        imm_next[11] = instr.b.imm11;
+                        imm_next[10:5] = instr.b.imm10_5;
+                        imm_next[4:1] = instr.b.imm4_1;
+
+                        ctrl_next.alu_op = ADDSUB;
+                        ctrl_next.alu_src1 = PC;
+                        ctrl_next.alu_src2 = IMM;
+
+                        ctrl_next.branch = 1;
+                        ctrl_next.branch_op = instr.b.funct3;
+                end
+
+                LOAD: begin
+                        imm_next[11:0] = instr.i.imm11_0;
+
+                        ctrl_next.alu_op = ADDSUB;
+                        ctrl_next.alu_src1 = RS1;
+                        ctrl_next.alu_src2 = IMM;
+
+                        ctrl_next.load = 1;
+                        ctrl_next.load_op = instr.i.funct3;
+                end
+
+                STORE: begin
+                        imm_next[11:5] = instr.s.imm11_5;
+                        imm_next[4:0] = instr.s.imm4_0;
+
+                        ctrl_next.alu_op = ADDSUB;
+                        ctrl_next.alu_src1 = RS1;
+                        ctrl_next.alu_src2 = IMM;
+
+                        ctrl_next.store = 1;
+                        ctrl_next.store_op = instr.s.funct3;
+
+                        ctrl_next.wb = 1;
+                        ctrl_next.wb_src = MEM;
+                end
+
+                ALUI: begin
+                        imm_next[11:0] = instr.i.imm11_0;
+
+                        ctrl_next.alu_op = instr.i.funct3;
+                        ctrl_next.alu_src1 = RS1;
+                        ctrl_next.alu_src2 = IMM;
+
+                        ctrl_next.wb = 1;
+                        ctrl_next.wb = ALU;
+                end
+
+                ALUR: begin
+                        ctrl_next.alu_op = instr.i.funct3;
+                        ctrl_next.alu_src1 = RS1;
+                        ctrl_next.alu_src2 = RS2;
+
+                        ctrl_next.wb = 1;
+                        ctrl_next.wb = ALU;
+                end
+
+                FENCE:;         // Single-core system; leaving as NOP for now
+                SYSTEM:;        // TODO: Add ctrl signalling
+        endcase 
+end
+
+// Save outputs to pipeline registers
+always_ff @(posedge clk or posedge rst)
+begin
+        if (rst | flush) begin
+                ctrl_o.opcode <= ALUI;
+                ctrl_o.alu_op <= ADDSUB;
+                rs1_o <= 0;
+                rs2_o <= 0;
+                imm_o <= 0;
+                rd_o <= 0;
+        end else if (~stall) begin
+                ctrl_o <= ctrl_next;
+                rs1_o <= rs1_next;
+                rs2_o <= rs2_next;
+                imm_o <= imm_next;
+                rd_o <= rd_next;
+        end
+end
+
+endmodule: decoder
---
+++ rtl/core/rv32.sv
@@ -0,0 +1,155 @@
+package rv32;
+
+        /*====================================================================*/
+        /*              ENUM DECLARATIONS FOR INSTR FIELDS                    */
+        /*====================================================================*/
+        typedef enum logic [6:0] {
+                LUI     = 7'b01101_11,
+                AUIPC   = 7'b00101_11,
+                JAL     = 7'b11011_11,
+                JALR    = 7'b11001_11,
+                BRANCH  = 7'b11000_11,
+                LOAD    = 7'b00000_11,
+                STORE   = 7'b01000_11,
+                ALUI    = 7'b00100_11,
+                ALUR    = 7'b01100_11,
+                FENCE   = 7'b00011_11,
+                SYSTEM  = 7'b11100_11
+        } opcode_e;
+
+        typedef enum logic [2:0] {
+                BEQ     = 3'b000,       BGE     = 3'b101,
+                BNE     = 3'b001,       BLTU    = 3'b110,
+                BLT     = 3'b100,       BGEU    = 3'b111
+        } branch_funct3_e;
+
+        typedef enum logic [2:0] {
+                LB    = 3'b000,         LBU     = 3'b100,
+                LH    = 3'b001,         LHU     = 3'b101,
+                LW    = 3'b010
+        } load_funct3_e;
+
+        typedef enum logic [2:0] {
+                SB    = 3'b000,
+                SH    = 3'b001,
+                SW    = 3'b010
+        } store_funct3_e;
+                
+        typedef enum logic [2:0] {
+                ADDSUB  = 3'b000,       XOR     = 3'b100,
+                SLL     = 3'b001,       SR      = 3'b101,
+                SLT     = 3'b010,       OR      = 3'b110,
+                SLTU    = 3'b011,       AND     = 3'b111
+        } alu_funct3_e;
+
+        typedef enum logic [6:0] {
+                ALT     = 7'b01_00000,
+                NORM    = 7'b00_00000
+        } funct7_e;
+
+        /*====================================================================*/
+        /*              SOURCE ENUMS                                          */
+        /*====================================================================*/
+        typedef enum logic {
+                RS1,
+                PC
+        } alu_src1_e;
+
+        typedef enum logic [1:0] {
+                RS2,
+                IMM,
+                FOUR,
+                ZERO
+        } alu_src2_e;
+
+        typedef enum logic [1:0] {
+                ALU,
+                MEM,
+                PC4
+        } wb_src_e;
+
+        /*====================================================================*/
+        /*              ENCODED INSTRUCTION TYPEDEF                           */
+        /*====================================================================*/
+        typedef union packed {
+                logic [31:0] raw;
+
+                struct packed {
+                        logic [6:0] funct7;
+                        logic [4:0] rs2;
+                        logic [4:0] rs1;
+                        logic [2:0] funct3;
+                        logic [4:0] rd;
+                        logic [6:0] opcode;
+                } r;
+
+                struct packed {
+                        logic [11:0] imm11_0;
+                        logic [4:0] rs1;
+                        logic [2:0] funct3;
+                        logic [4:0] rd;
+                        logic [6:0] opcode;
+                } i;
+
+                struct packed {
+                        logic [6:0] imm11_5;
+                        logic [4:0] rs2;
+                        logic [4:0] rs1;
+                        logic [2:0] funct3;
+                        logic [4:0] imm4_0;
+                        logic [6:0] opcode;
+                } s;
+
+                struct packed {
+                        logic imm12;
+                        logic [5:0] imm10_5;
+                        logic [4:0] rs2;
+                        logic [4:0] rs1;
+                        logic [2:0] funct3;
+                        logic [3:0] imm4_1;
+                        logic imm11;
+                        logic [6:0] opcode;
+                } b;
+
+                struct packed {
+                        logic [19:0] imm31_12;
+                        logic [4:0] rd;
+                        logic [6:0] opcode;
+                } u;
+
+                struct packed {
+                        logic imm20;
+                        logic [9:0] imm10_1;
+                        logic imm11;
+                        logic [7:0] imm19_12;
+                        logic [4:0] rd;
+                        logic [6:0] opcode;
+                } j;
+        } instr_t;
+
+        /*====================================================================*/
+        /*              DECODED INSTRUCTION TYPEDEF                           */
+        /*====================================================================*/
+        typedef struct packed {
+                opcode_e opcode;
+
+                alu_funct3_e alu_op;
+                alu_src1_e alu_src1;
+                alu_src2_e alu_src2;
+
+                logic branch;
+                branch_funct3_e branch_op;
+
+                logic jump;
+
+                logic load;
+                load_funct3_e load_op;
+
+                logic store;
+                store_funct3_e store_op;
+
+                logic wb;
+                wb_src_e wb_src;
+        } ctrl_t;
+
+endpackage: rv32
