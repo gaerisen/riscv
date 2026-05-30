a796a6c8ac5d216f9098e20735d56250cc25a7f0
Garrison Taylor <garrison.m.taylor@gmail.com>
2026-05-30 10:37:35 -0500

    Finish version 1 of fetch.sv
    Clean up the main FSM, handling valid_o and instr_o separately. Remove
    all always_latch blocks in favor of the more consistent always_ff

--- rtl/fetch.sv
+++ rtl/fetch.sv
@@ -1,7 +1,11 @@
 module fetch
 import rv32::*;
-#()
-(
+#(
+        parameter int RAS_SIZE = 8,
+        parameter int RAS_PTR_SIZE = $clog2(RAS_SIZE),
+        parameter int B_PRED_SIZE = 256,
+        parameter int B_PRED_PTR_SIZE = $clog2(B_PRED_SIZE)
+)(
         input clk,
         input rst,
 
@@ -11,14 +15,14 @@ import rv32::*;
 
         output logic [31:0] i_addr,
 
-        // Jump/branch signals (From EXE; PC - 2)
+        // Jump/branch signals (From EXE; pc_o - 2)
         input [31:0] pc_from_exe,
         input jump,
         input branch,
         input branch_taken,
         input [31:0] alu_result,
 
-        // System control signals (From DEC; PC - 1)
+        // System control signals (From DEC; pc_o - 1)
         input [31:0] pc_from_dec,
         input system_t system_word,
         input [31:0] mtvec,
@@ -31,24 +35,30 @@ import rv32::*;
         output logic flush_o
 );
 
+initial
+begin
+        $dumpfile("fetch.vcd");
+        $dumpvars(0, fetch);
+end
+
 typedef enum logic [2:0] {
-        WAITING_IMEM,
-        WAITING_IMEM_SPEC,
-        WAITING_JALR,
-        STREAMING,
-        SPECULATING
+        WAITING_JALR = 0,
+        STREAMING = 1,
+        SPECULATING = 2
 } state_e;
 
 state_e state;
 state_e state_next;
 
 logic [31:0] pc_next;
+
 logic [31:0] instr_next;
 logic valid_next;
 logic flush_next;
 
 // Prediction signals
 logic inst_is_jump;
+logic inst_is_jalr;
 logic inst_is_call;
 logic inst_is_ret;
 logic inst_is_b;
@@ -57,15 +67,21 @@ logic [31:0] jump_target;
 
 logic b_predict_taken;
 
-// Prediction/target signals
-logic [1:0] b_pred [0:255];
-logic [31:0] btb [0:255];
-logic [31:0] ras [0:7];
-logic [2:0] ras_ptr;
-logic [2:0] ras_ptr_next;
+logic mispredict;
 
+// Prediction registers
+logic [1:0] b_pred [0:B_PRED_SIZE-1];
+logic [31:0] btb [0:B_PRED_SIZE-1];
 
+logic [31:0] ras [0:RAS_SIZE-1];
+logic [RAS_PTR_SIZE-1:0] ras_ptr;
+logic [RAS_PTR_SIZE-1:0] ras_ptr_next;
+
+// Slam IMEM with whatever's next
 assign i_addr = pc_next;
+
+// Detect any kind of misprediction
+assign mispredict = (branch | jump) & (pc_from_dec != alu_result);
 
 
 //=================================
@@ -88,8 +104,6 @@ assign inst_is_jalr = (instr_o.i.opcode == JALR);
 assign inst_is_ret = inst_is_jalr & (instr_o.i.rs1 == 1) &
                         (instr_o.i.imm11_0 == 0);
 
-assign ret_mispredicted = jump & (pc_from_dec != alu_result);
-
 // Push/pop on call/ret
 always_comb
 begin
@@ -103,17 +117,20 @@ end
 
 always_ff @(posedge clk or posedge rst)
 begin
-        if (rst)
+        if (rst) begin
                 ras_ptr <= 0;
-        else
+                
+                for (int i = 0; i < RAS_SIZE - 1; i++) begin
+                        ras[i] <= 0;
+                end
+        end
+        else begin
                 ras_ptr <= ras_ptr_next;
-end
+                if (inst_is_call) begin
+                        ras[ras_ptr_next] = pc_o + 4;
+                end
+        end
 
-// Latch return addresses in RAS
-always_latch
-begin
-        if (inst_is_call)
-                ras[ras_ptr_next] = pc_o + 4;
 end
 
 //================================
@@ -126,28 +143,34 @@ assign inst_is_b = (instr_o.b.opcode == BRANCH);
 // Read branch predictor high bit for current PC
 assign b_predict_taken = inst_is_b & b_pred[pc_o[9:2]][1];
 
-assign b_mispredicted = branch & (pc_from_dec != alu_result);
-
 // Update predictor and BTB for every branch executed
-always_latch
+always_ff @(posedge clk or posedge rst)
 begin
-        if (branch & branch_taken) begin
-                unique case (b_pred[pc_from_exe[9:2]])
-                2'b00: b_pred[pc_from_exe[9:2]] = 2'b01;
-                2'b01: b_pred[pc_from_exe[9:2]] = 2'b10;
-                2'b10: b_pred[pc_from_exe[9:2]] = 2'b11;
-                2'b11: b_pred[pc_from_exe[9:2]] = 2'b11;
-                endcase
-                
-                btb[pc_from_exe[9:2]] = alu_result;
+        if (rst) begin
+                for (int i = 0; i < B_PRED_SIZE - 1; i++) begin
+                        b_pred[i] <= 0;
+                        btb[i] <= 0;
+                end
         end
-        else if (branch & ~branch_taken) begin
-                unique case (b_pred[pc_from_exe[9:2]])
-                2'b00: b_pred[pc_from_exe[9:2]] = 2'b00;
-                2'b01: b_pred[pc_from_exe[9:2]] = 2'b00;
-                2'b10: b_pred[pc_from_exe[9:2]] = 2'b01;
-                2'b11: b_pred[pc_from_exe[9:2]] = 2'b10;
-                endcase
+        else begin
+                if (branch & branch_taken) begin
+                        unique case (b_pred[pc_from_exe[B_PRED_PTR_SIZE+1:2]])
+                        2'b00: b_pred[pc_from_exe[B_PRED_PTR_SIZE+1:2]] = 2'b01;
+                        2'b01: b_pred[pc_from_exe[B_PRED_PTR_SIZE+1:2]] = 2'b10;
+                        2'b10: b_pred[pc_from_exe[B_PRED_PTR_SIZE+1:2]] = 2'b11;
+                        2'b11: b_pred[pc_from_exe[B_PRED_PTR_SIZE+1:2]] = 2'b11;
+                        endcase
+                        
+                        btb[pc_from_exe[B_PRED_PTR_SIZE+1:2]] = alu_result;
+                end
+                else if (branch & ~branch_taken) begin
+                        unique case (b_pred[pc_from_exe[B_PRED_PTR_SIZE+1:2]])
+                        2'b00: b_pred[pc_from_exe[B_PRED_PTR_SIZE+1:2]] = 2'b00;
+                        2'b01: b_pred[pc_from_exe[B_PRED_PTR_SIZE+1:2]] = 2'b00;
+                        2'b10: b_pred[pc_from_exe[B_PRED_PTR_SIZE+1:2]] = 2'b01;
+                        2'b11: b_pred[pc_from_exe[B_PRED_PTR_SIZE+1:2]] = 2'b10;
+                        endcase
+                end
         end
 end
 
@@ -159,176 +182,122 @@ end
 always_comb
 begin
         // Stall-safe defaults
-        pc_next = pc_o;
-        instr_next = 0;
-        valid_next = 0;
+        pc_next = pc_o + 4;
         flush_next = 0;
         state_next = state;
 
-        if (~i_data_ready) begin
-                if (state == SPECULATING) begin
-                        if (
-        end
-        else begin
-        end
-        
-
         unique case (state)
         STREAMING: begin
+                // Entry point for all redirects; if imem isn't ready, hold
+                // i_addr at the redirect vector
                 if (~i_data_ready) begin
-                        instr_next = 0;
-                        valid_next = 0;
-                        state_next = WAITING_IMEM;
-
-                // All following combinations SHOULD be mutually exclusive; each
-                // relies on a unique opcode in current inst. Any order works.
+                        pc_next = pc_o;
                 end
+
+                // Next cases should all be mutually exclusive; each relies on a
+                // unique opcode in current inst. Any order works.
                 else if (inst_is_jump) begin
-                        pc_next = jump_target;  // Directly calculated -->
-                                                // No speculation
-                        if (~i_data_ready) begin
-                                valid_next = 0;
-                                instr_next = 0;
-                                state_next = WAITING_IMEM;
-                        end
+                        pc_next = jump_target;  // Directly calculated. No spec
                 end
                 else if (inst_is_b) begin
                         if (b_predict_taken) begin
                                 pc_next = btb[pc_o[9:2]];
                         end
-
-                        if (~i_data_ready) begin
-                                valid_next = 0;
-                                instr_next = 0;
-                                state_next = WAITING_IMEM_SPEC;
-                        end
-                        else begin
-                                state_next = SPECULATING;
-                        end
+                        state_next = SPECULATING;
                 end
                 else if (inst_is_ret) begin
                         pc_next = ras[ras_ptr];
-                        if (~i_data_ready) begin
-                                valid_next = 0;
-                                instr_next = 0;
-                                state_next = WAITING_IMEM_SPEC;
-                        end
-                        else begin
-                                state_next = SPECULATING;
-                        end
-
-                // Must come after ret check; only stall if nothing to predict
+                        state_next = SPECULATING;
                 end
+                // Must come after ret check; only stall if nothing to predict
                 else if (inst_is_jalr) begin
-                        instr_next = 0;
-                        valid_next = 0;
                         state_next = WAITING_JALR;
                 end
 
-        end
-
-        WAITING_IMEM: begin
-                if (i_data_ready) begin
-                        state_next = STREAMING;
-                end
-                else begin
-                        pc_next = pc_o;
-                        instr_next = 0;
-                        valid_next = 0;
-                        state_next = WAITING_IMEM;
-                end
-        end
-
-        WAITING_IMEM_SPEC: begin
-                if (i_data_ready) begin
-                        state_next = SPECULATING;
-                end
-                else begin
-                        pc_next = pc_o;
-                        instr_next = 0;
-                        valid_next = 0;
-                        state_next = WAITING_IMEM;
-                end
         end
 
         WAITING_JALR: begin
                 if (jump) begin
                         pc_next = alu_result;
-                        if (~i_data_ready) begin
-                                valid_next = 0;
-                                instr_next = 0;
-                                state_next = WAITING_IMEM;
-                        end
-                        else begin
-                                state_next = STREAMING;
-                        end
+                        state_next = STREAMING;
                 end
-                else begin
-                        pc_next = pc_o;
-                        instr_next = 0;
-                        valid_next = 0;
-                        state_next = WAITING_JALR;
+                // Spec checks in case we entered from SPECULATING. If this
+                // condition never gets flagged, we know that speculation
+                // succeeded, so the above exit point to STREAMING works.
+                else if (mispredict) begin
+                        pc_next = alu_result;
+                        flush_next = 1;
+                        state_next = STREAMING;
                 end
         end
 
         SPECULATING: begin
-                if (~i_data_ready) begin
-                        valid_next = 0;
-                        instr_next = 0;
-                        state_next = WAITING_IMEM_SPEC;
-                end
-                else if (branch & (pc_from_dec != alu_result)) begin // b_pred failed
+                // Speculation has failed
+                if (mispredict) begin
                         pc_next = alu_result;
                         flush_next = 1;
-                        if (~i_data_ready) begin
-                                valid_next = 0;
-                                instr_next = 0;
-                                state_next = WAITING_IMEM;
-                        end
-                        else begin
-                                valid_next = 1;
-                                state_next = STREAMING;
+                        state_next = STREAMING;
+                end
+                // Speculation completed successfully
+                else if (jump | branch) begin
+                        state_next = STREAMING;
+                end
+
+                // After speculation checks, behave exactly like STREAMING:
+                else if (inst_is_jump) begin
+                        pc_next = jump_target;  // Directly calculated. No spec
+                end
+                else if (inst_is_b) begin
+                        if (b_predict_taken) begin
+                                pc_next = btb[pc_o[9:2]];
                         end
                 end
-                else if (branch) begin // b_pred succeeded
-                        if (~i_data_ready) begin
-                                valid_next = 0;
-                                instr_next = 0;
-                                state_next = WAITING_IMEM;
-                        end
-                        else begin
-                                valid_next = 1;
-                                state_next = STREAMING;
-                        end
+                else if (inst_is_ret) begin
+                        pc_next = ras[ras_ptr];
                 end
-                else if (jump & (pc_from_dec != alu_result)) begin // ras failed
-                end
-                else if (jump) begin // ras succeeded
-                        if (~i_data_ready) begin
-                                valid_next = 0;
-                                instr_next = 0;
-                                state_next = WAITING_IMEM;
-                        end
-                        else begin
-                                valid_next = 1;
-                                state_next = STREAMING;
-                        end
-                end
-                else begin // still speculating
-                        // TODO: Stall if we hit another speculative instr
-                        state_next = SPECULATING;
+                // Must come after ret check; only stall if nothing to predict
+                else if (inst_is_jalr) begin
+                        state_next = WAITING_JALR;
                 end
         end
         endcase
+
+        // If system-level redirect, clobber FSM results completely
+        if (system_word.illegal | system_word.ecall | system_word.ebreak) begin
+                pc_next = mtvec;
+                flush_next = 1;
+                state_next = STREAMING;
+        end
+        else if (system_word.mret) begin
+                pc_next = mepc;
+                flush_next = 1;
+                state_next = STREAMING;
+        end
+
+        // Instruction validity detection
+        valid_next = 1;
+        instr_next = i_data_i;
+
+        if (~i_data_ready | state_next == WAITING_JALR) begin
+                valid_next = 0;
+                instr_next = 0;
+        end
 end
 
+// Instruction validity detection
+always_comb
+begin
+end
+
+// Output flip-flops
 always_ff @(posedge clk or posedge rst)
 begin
         if (rst) begin
-                pc_o <= 0;
+                pc_o <= -4;
                 instr_o <= 0;
                 valid_o <= 0;
                 flush_o <= 0;
+
                 state <= STREAMING;
         end
         else begin
@@ -336,6 +305,7 @@ begin
                 instr_o <= instr_next;
                 valid_o <= valid_next;
                 flush_o <= flush_next;
+
                 state <= state_next;
         end
 end
